3bffae5664fce84b98c7088cccad9d3da09f668e
Thiago Alexandre <thiagoalex.desouza@gmail.com>
2019-07-18 18:51:57 -0300

    Ciração das entidades

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaDePagamento.cs
@@ -0,0 +1,34 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaDePagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto
+        {
+            get { return Id == (int)TipoFormaDePagamento.Boleto; }
+        }
+
+        public bool EhCredito
+        {
+            get { return Id == (int)TipoFormaDePagamento.Credito; }
+        }
+
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaDePagamento.Deposito; }
+        }
+
+        public bool EhNaoDefinido
+        {
+            get { return Id == (int)TipoFormaDePagamento.NaoDefinido; }
+        }
+    }
+}
